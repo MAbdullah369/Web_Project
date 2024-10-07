e747e6b67897f1c524c0236450afd28e390f7cce
MAbdullah369 <bscs22111@itu.edu.pk>
2024-10-07 19:25:02 +0500

    Search-bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import Navbar from './Navbar/Navbar'
+import SearchBar from './search-bar/SearchBar'
 
 function App() {
 
   return (
     <>
       <Navbar/>
-    
+      <SearchBar/>
     </>
   )
 }
