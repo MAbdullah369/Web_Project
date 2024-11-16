97ab270e49519e20953fac32c3ad56f7d24a0d10
MAbdullah369 <bscs22111@itu.edu.pk>
2024-11-17 01:27:49 +0500

    Cards-Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './Navbar/Navbar'
 import SearchBar from './search-bar/SearchBar'
 import Categories from './Categories/Categories'
+import ListingCards from './ListingCards/ListingCards'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <SearchBar/>
       <Categories/>
+      <ListingCards/>
     </>
   )
 }
---
+++ src/ListingCards/ListingCards.jsx
@@ -0,0 +1,229 @@
+import React from 'react'
+import './ListingCards.css'
+
+const ListingCards = () => {
+
+  return (
+    <div>
+        <div class="card-grid">
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/ml/photo_enhancement/pictures/miso/Hosting-42685161/original/52b9c580-e69a-47a1-9a52-be14b3ef2ddf.jpeg?im_w=720&im_q=highq" alt="Cozy Chalet"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3> Colroy-la-Grande, France </h3>
+        <p>Viewed 3,277 times last week
+<br />Nov 23 – 28</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/ml/photo_enhancement/pictures/miso/Hosting-24541146/original/d2624aaf-c1c7-477e-827f-081f3340aa16.jpeg" alt="Windmill"/>
+      </div>
+      <div class="card-details">
+        <h3>Windmill</h3>
+        <p>Netherlands</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/pictures/c397f0b2-8eab-4902-b30f-d552dc8b06dd.jpg?im_w=720&im_q=highq" alt="Seaside Cottage"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3>Seaside Cottage</h3>
+        <p>Norway</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/pictures/miso/Hosting-624892699755656283/original/80eb68f7-f7f8-4cd5-a2cc-b6fc423d9178.jpeg?im_w=720&im_q=highq" alt="Game Room"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3>Game Room</h3>
+        <p>Florida, USA</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/ml/photo_enhancement/pictures/miso/Hosting-42685161/original/52b9c580-e69a-47a1-9a52-be14b3ef2ddf.jpeg?im_w=720&im_q=highq" alt="Cozy Chalet"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3> Colroy-la-Grande, France </h3>
+        <p>Viewed 3,277 times last week
+<br />Nov 23 – 28</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/ml/photo_enhancement/pictures/miso/Hosting-24541146/original/d2624aaf-c1c7-477e-827f-081f3340aa16.jpeg" alt="Windmill"/>
+      </div>
+      <div class="card-details">
+        <h3>Windmill</h3>
+        <p>Netherlands</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/pictures/c397f0b2-8eab-4902-b30f-d552dc8b06dd.jpg?im_w=720&im_q=highq" alt="Seaside Cottage"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3>Seaside Cottage</h3>
+        <p>Norway</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/pictures/miso/Hosting-624892699755656283/original/80eb68f7-f7f8-4cd5-a2cc-b6fc423d9178.jpeg?im_w=720&im_q=highq" alt="Game Room"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3>Game Room</h3>
+        <p>Florida, USA</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/ml/photo_enhancement/pictures/miso/Hosting-42685161/original/52b9c580-e69a-47a1-9a52-be14b3ef2ddf.jpeg?im_w=720&im_q=highq" alt="Cozy Chalet"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3> Colroy-la-Grande, France </h3>
+        <p>Viewed 3,277 times last week
+<br />Nov 23 – 28</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/ml/photo_enhancement/pictures/miso/Hosting-24541146/original/d2624aaf-c1c7-477e-827f-081f3340aa16.jpeg" alt="Windmill"/>
+      </div>
+      <div class="card-details">
+        <h3>Windmill</h3>
+        <p>Netherlands</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/pictures/c397f0b2-8eab-4902-b30f-d552dc8b06dd.jpg?im_w=720&im_q=highq" alt="Seaside Cottage"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3>Seaside Cottage</h3>
+        <p>Norway</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/pictures/miso/Hosting-624892699755656283/original/80eb68f7-f7f8-4cd5-a2cc-b6fc423d9178.jpeg?im_w=720&im_q=highq" alt="Game Room"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3>Game Room</h3>
+        <p>Florida, USA</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/ml/photo_enhancement/pictures/miso/Hosting-42685161/original/52b9c580-e69a-47a1-9a52-be14b3ef2ddf.jpeg?im_w=720&im_q=highq" alt="Cozy Chalet"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3> Colroy-la-Grande, France </h3>
+        <p>Viewed 3,277 times last week
+<br />Nov 23 – 28</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/ml/photo_enhancement/pictures/miso/Hosting-24541146/original/d2624aaf-c1c7-477e-827f-081f3340aa16.jpeg" alt="Windmill"/>
+      </div>
+      <div class="card-details">
+        <h3>Windmill</h3>
+        <p>Netherlands</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/pictures/c397f0b2-8eab-4902-b30f-d552dc8b06dd.jpg?im_w=720&im_q=highq" alt="Seaside Cottage"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3>Seaside Cottage</h3>
+        <p>Norway</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/pictures/miso/Hosting-624892699755656283/original/80eb68f7-f7f8-4cd5-a2cc-b6fc423d9178.jpeg?im_w=720&im_q=highq" alt="Game Room"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3>Game Room</h3>
+        <p>Florida, USA</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/ml/photo_enhancement/pictures/miso/Hosting-42685161/original/52b9c580-e69a-47a1-9a52-be14b3ef2ddf.jpeg?im_w=720&im_q=highq" alt="Cozy Chalet"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3> Colroy-la-Grande, France </h3>
+        <p>Viewed 3,277 times last week
+<br />Nov 23 – 28</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/ml/photo_enhancement/pictures/miso/Hosting-24541146/original/d2624aaf-c1c7-477e-827f-081f3340aa16.jpeg" alt="Windmill"/>
+      </div>
+      <div class="card-details">
+        <h3>Windmill</h3>
+        <p>Netherlands</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/pictures/c397f0b2-8eab-4902-b30f-d552dc8b06dd.jpg?im_w=720&im_q=highq" alt="Seaside Cottage"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3>Seaside Cottage</h3>
+        <p>Norway</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-image">
+        <img src="https://a0.muscache.com/im/pictures/miso/Hosting-624892699755656283/original/80eb68f7-f7f8-4cd5-a2cc-b6fc423d9178.jpeg?im_w=720&im_q=highq" alt="Game Room"/>
+        <span class="badge">Guest favorite</span>
+      </div>
+      <div class="card-details">
+        <h3>Game Room</h3>
+        <p>Florida, USA</p>
+      </div>
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default ListingCards
